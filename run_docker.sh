db6900020a39f67b2754f659d0c3b413e2c26c1f
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-04-16 13:44:23 +0200

    try again

---
+++ run_docker.sh
@@ -0,0 +1,4 @@
+docker pull quay.io/pypa/manylinux1_x86_64
+docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
+docker pull quay.io/pypa/manylinux1_i686
+docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
