7d4902a7259715e5ba7edd2f394541621705cd8b
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-04-16 14:30:06 +0200

    Modify travis instructions again

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,6 @@
-docker pull quay.io/pypa/manylinux1_x86_64
-docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
-docker pull quay.io/pypa/manylinux1_i686
-docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
+if [ -z $MULTIDICT_NO_EXTENSIONS ]; then
+    docker pull quay.io/pypa/manylinux1_x86_64
+    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
+    docker pull quay.io/pypa/manylinux1_i686
+    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
+fi
