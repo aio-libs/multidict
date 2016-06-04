f09f39ddaaf8ae401f25a68806bafd413811d7bc
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-06-04 12:15:07 -0700

    Make uploading script more verbose

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -10,7 +10,7 @@ import os
 __all__ = ('MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr', '__version__')
 
-__version__ = '1.1.0b1'
+__version__ = '1.1.0b2'
 
 
 if bool(os.environ.get('MULTIDICT_NO_EXTENSIONS')):
--- run_docker.sh
+++ run_docker.sh
@@ -3,8 +3,12 @@ if [ -z $MULTIDICT_NO_EXTENSIONS ]; then
     docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
     docker pull quay.io/pypa/manylinux1_i686
     docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
+    echo "Dist folder content is:"
+    ls dist
 fi
 
 if [ -n $TRAVIS_TAG ]; then
+    echo "Upload dists to PyPI"
+    ls
     python -m twine upload dist/* --username andrew.svetlov --password $PYPI_PASSWD
 fi
