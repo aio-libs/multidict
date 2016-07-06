ea306f22c4b04d7ff585205dd2492fde5b5ae616
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-06 19:45:05 +0300

    Bump to 1.1.0b8

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 PYTHON_VERSIONS="cp34-cp34m cp35-cp35m"
 
-# Compile wheels
+echo "Compile wheels"
 for PYTHON in ${PYTHON_VERSIONS}; do
     /opt/python/${PYTHON}/bin/pip install -r /io/requirements-wheel.txt
     /opt/python/${PYTHON}/bin/pip wheel /io/ -w /io/dist/
 done
 
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    if [[ $whl == *"linux_"* ]]; then
-       auditwheel repair $whl -w /io/dist/
-    fi
+echo "Bundle external shared libraries into the wheels"
+for whl in /io/dist/*.whl; do
+    auditwheel repair $whl -w /io/dist/
 done
 
-# Install packages and test
+echo "Install packages and test"
 for PYTHON in ${PYTHON_VERSIONS}; do
     /opt/python/${PYTHON}/bin/pip install multidict --no-index -f file:///io/dist
     rm -rf /io/tests/__pycache__
