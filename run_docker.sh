31b3b07c7f9ccd1d53b2d933e44e2136fb8b21d2
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-06 19:07:53 +0300

    Bump to 1.1.0b6

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,7 @@
 if [ -z $MULTIDICT_NO_EXTENSIONS ]; then
     docker pull quay.io/pypa/manylinux1_x86_64
     docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
+
     docker pull quay.io/pypa/manylinux1_i686
     docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
     echo "Dist folder content is:"
@@ -10,7 +11,18 @@ fi
 if [ ! -z $TRAVIS_TAG ]; then
     echo "Upload dists to PyPI"
     ls
-    python -m twine upload dist/* --username andrew.svetlov --password $PYPI_PASSWD
+    for f in dist/*.whl
+    do
+        if [[ f == *"linux_x86_64"* ]]
+        then
+            docker run --rm -v `pwd`:/io auditwheel quay.io/pypa/manylinux1_x86_64 repair $f
+        fi
+        if [[ f == *"linux_i686"* ]]
+        then
+            docker run --rm -v `pwd`:/io auditwheel quay.io/pypa/manylinux1_i686 linux32 repair $f
+        fi
+        python -m twine upload $f --username andrew.svetlov --password $PYPI_PASSWD
+    done
 fi
 
 
