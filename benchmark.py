76000c35c68bffade9d97f47f5a2dcbe25353eb7
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-16 16:45:08 +0300

    Extend benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,4 @@
+import gc
 import timeit
 
 
@@ -44,22 +45,44 @@ for i in range(20):
 key = upstr('key10')
 """
 
+upstr_from_upstr = """\
+upstr(val)
+"""
+
+make_upstr = """\
+val = upstr('VaLuE')
+"""
+
 print("Cython setitem str: {:.3f} sec".format(
     timeit.timeit(setitem, cython_multidict+fill)))
+gc.collect()
 
 print("Python setitem str: {:.3f} sec".format(
     timeit.timeit(setitem, python_multidict+fill)))
+gc.collect()
 
 
 print("Cython getitem str: {:.3f} sec".format(
     timeit.timeit(getitem, cython_multidict+fill)))
+gc.collect()
 
 print("Python getitem str: {:.3f} sec".format(
     timeit.timeit(getitem, python_multidict+fill)))
+gc.collect()
 
 
 print("Cython getitem upstr: {:.3f} sec".format(
     timeit.timeit(getitem, cython_cimultidict+fill)))
+gc.collect()
 
 print("Python getitem upstr: {:.3f} sec".format(
     timeit.timeit(getitem, python_cimultidict+fill)))
+gc.collect()
+
+print("Cython upstr from upstr: {:.3f} sec".format(
+    timeit.timeit(upstr_from_upstr, cython_cimultidict+make_upstr)))
+gc.collect()
+
+print("Python upstr from upstr: {:.3f} sec".format(
+    timeit.timeit(upstr_from_upstr, python_cimultidict+make_upstr)))
+gc.collect()
