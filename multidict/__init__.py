24fc0addcc110aab33206516890dbb125915d1d4
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-21 18:37:40 +0200

    Fix travis

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -10,7 +10,7 @@ import os
 __all__ = ('MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr')
 
-__version__ = '1.3.0a0'
+__version__ = '1.3.0a1'
 
 
 if bool(os.environ.get('MULTIDICT_NO_EXTENSIONS')):
