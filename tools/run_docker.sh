e2fc57c5877f60307af6c8855ca3303c97cef9cc
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-07-15 16:19:09 +0300

    Fix awaiting of async docker pull to get images

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -6,21 +6,22 @@ then
     exit 1
 fi
 
+manylinux1_image_prefix="quay.io/pypa/manylinux1_"
 dock_ext_args=""
 
 for arch in x86_64 i686
 do
-    docker pull "quay.io/pypa/manylinux1_${arch}" &
-    docker_pull_pid_${arch}=$!
+    docker pull "${manylinux1_image_prefix}${arch}" &
+    export docker_pull_pid_${arch}=$!
 done
 
 for arch in x86_64 i686
 do
-    wait docker_pull_pid_${arch}
+    wait '$docker_pull_pid_'${arch}`  # await for docker image for current arch to be pulled from hub
     [ $arch == "i686" ] && dock_ext_args="linux32"
 
     echo Building wheel for $arch arch
-    docker run --rm -v `pwd`:/io "quay.io/pypa/manylinux1_${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
+    docker run --rm -v `pwd`:/io "${manylinux1_image_prefix}${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
 
     dock_ext_args=""  # Reset docker args, just in case
 done
