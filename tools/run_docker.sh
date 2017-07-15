897336241eae21f10f897ce452add6f1718dd984
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-07-15 15:59:41 +0300

    Parallelize pulling of x86_64 & i686 docker images

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -10,10 +10,16 @@ dock_ext_args=""
 
 for arch in x86_64 i686
 do
+    docker pull "quay.io/pypa/manylinux1_${arch}" &
+    docker_pull_pid_${arch}=$!
+done
+
+for arch in x86_64 i686
+do
+    wait docker_pull_pid_${arch}
     [ $arch == "i686" ] && dock_ext_args="linux32"
 
     echo Building wheel for $arch arch
-    docker pull "quay.io/pypa/manylinux1_${arch}"
     docker run --rm -v `pwd`:/io "quay.io/pypa/manylinux1_${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
 
     dock_ext_args=""  # Reset docker args, just in case
