9d6bae368e0ca74364cbfcebeb687b6e8ebe3f5e
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-07-25 18:24:52 +0300

    Bump to 3.1.4a3

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -10,7 +10,7 @@ import os
 __all__ = ('MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr', 'istr')
 
-__version__ = '3.1.4a2'
+__version__ = '3.1.4a3'
 
 
 if bool(os.environ.get('MULTIDICT_NO_EXTENSIONS')):
