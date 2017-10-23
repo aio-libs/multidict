188942240cf30f163675bb2a7e8234f07ab9bb0d
Alex Khomchenko <akhomchenko@gmail.com>
2017-10-23 12:19:28 +0200

    Refactor unit tests to pytest style (#167)
    * moved multidict tests to test_mutable_multidict
    * moved types tests to test_types

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,21 @@
 import pickle
 
+import pytest
+
+from multidict._compat import USE_CYTHON_EXTENSIONS
+
+
+OPTIONAL_CYTHON = (() if USE_CYTHON_EXTENSIONS else
+                   pytest.mark.skip(reason='No Cython extensions available'))
+
+
+@pytest.fixture(scope='session', params=[
+    pytest.param('multidict._multidict', marks=OPTIONAL_CYTHON),
+    'multidict._multidict_py'
+])
+def _multidict(request):
+    return pytest.importorskip(request.param)
+
 
 def pytest_generate_tests(metafunc):
     if 'pickle_protocol' in metafunc.fixturenames:
---
+++ tests/test_types.py
@@ -0,0 +1,55 @@
+import pytest
+
+
+def test_proxies(_multidict):
+    assert issubclass(_multidict.CIMultiDictProxy, _multidict.MultiDictProxy)
+
+
+def test_dicts(_multidict):
+    assert issubclass(_multidict.CIMultiDict, _multidict.MultiDict)
+
+
+def test_proxy_not_inherited_from_dict(_multidict):
+    assert not issubclass(_multidict.MultiDictProxy, _multidict.MultiDict)
+
+
+def test_dict_not_inherited_from_proxy(_multidict):
+    assert not issubclass(_multidict.MultiDict, _multidict.MultiDictProxy)
+
+
+def test_create_multidict_proxy_from_nonmultidict(_multidict):
+    with pytest.raises(TypeError):
+        _multidict.MultiDictProxy({})
+
+
+def test_create_multidict_proxy_from_cimultidict(_multidict):
+    d = _multidict.CIMultiDict(key='val')
+    p = _multidict.MultiDictProxy(d)
+    assert p == d
+
+
+def test_create_multidict_proxy_from_multidict_proxy_from_mdict(_multidict):
+    d = _multidict.MultiDict(key='val')
+    p = _multidict.MultiDictProxy(d)
+    assert p == d
+    p2 = _multidict.MultiDictProxy(p)
+    assert p2 == p
+
+
+def test_create_cimultidict_proxy_from_cimultidict_proxy_from_ci(_multidict):
+    d = _multidict.CIMultiDict(key='val')
+    p = _multidict.CIMultiDictProxy(d)
+    assert p == d
+    p2 = _multidict.CIMultiDictProxy(p)
+    assert p2 == p
+
+
+def test_create_cimultidict_proxy_from_nonmultidict(_multidict):
+    with pytest.raises(TypeError):
+        _multidict.CIMultiDictProxy({})
+
+
+def test_create_ci_multidict_proxy_from_multidict(_multidict):
+    d = _multidict.MultiDict(key='val')
+    with pytest.raises(TypeError):
+        _multidict.CIMultiDictProxy(d)
