fc4f2ba3b02ef2d841ce759f03a9fc9b0cc8b953
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-05-06 21:25:46 +0300

    Bump to 4.3.1

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -12,7 +12,7 @@ __all__ = ('MultiMapping', 'MutableMultiMapping',
            'MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr', 'istr')
 
-__version__ = '4.3.0'
+__version__ = '4.3.1'
 
 
 from ._abc import MultiMapping, MutableMultiMapping
