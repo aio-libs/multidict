ec0df9991cd4a09302632e476c4af83f209745c9
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-08-02 13:24:27 +0200

    🔖📦 Bump to v4.4.0a2 and release it to PYPI

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -12,7 +12,7 @@ __all__ = ('MultiMapping', 'MutableMultiMapping',
            'MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr', 'istr')
 
-__version__ = '4.4.0a1'
+__version__ = '4.4.0a2'
 
 
 from ._abc import MultiMapping, MutableMultiMapping
