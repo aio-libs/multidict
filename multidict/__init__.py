96697e7c4bf7f83ae90eb955d7f03fe22ee87cc8
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-09-04 00:34:50 +0200

    Bump v4.4.0a42

--- multidict/__init__.py
+++ multidict/__init__.py
@@ -12,7 +12,7 @@ __all__ = ('MultiMapping', 'MutableMultiMapping',
            'MultiDictProxy', 'CIMultiDictProxy',
            'MultiDict', 'CIMultiDict', 'upstr', 'istr')
 
-__version__ = '4.4.0a41'
+__version__ = '4.4.0a42'
 
 
 from ._abc import MultiMapping, MutableMultiMapping
