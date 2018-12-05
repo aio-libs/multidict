e0493d89bd5971f8dd6a6f2c05cda0ca696037db
Igor Emelianov <cybercjp@gmail.com>
2018-12-05 22:19:26 +0200

    Fix tp_flags.

--- multidict/_multidict_c.c
+++ multidict/_multidict_c.c
@@ -736,8 +736,7 @@ static PyTypeObject multidict_type = {
     0,                                               /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT
         | Py_TPFLAGS_BASETYPE
-        | Py_TPFLAGS_HAVE_GC
-        | Py_TPFLAGS_HAVE_FINALIZE,                  /* tp_flags */
+        | Py_TPFLAGS_HAVE_GC,                        /* tp_flags */
     0,                                               /* tp_doc */
     0,                                               /* tp_traverse */
     0,                                               /* tp_clear */
