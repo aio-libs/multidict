6f93a790e5e60e742131c89fe89bbd7dec24fbff
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-11-14 01:54:59 +0200

    Fix tests

--- multidict/_compat.py
+++ multidict/_compat.py
@@ -6,3 +6,9 @@ NO_EXTENSIONS = bool(os.environ.get("MULTIDICT_NO_EXTENSIONS"))
 PYPY = platform.python_implementation() == "PyPy"
 
 USE_CYTHON_EXTENSIONS = USE_CYTHON = not NO_EXTENSIONS and not PYPY
+
+if USE_CYTHON_EXTENSIONS:
+    try:
+        from . import _multidict  # noqa
+    except ImportError:
+        USE_CYTHON_EXTENSIONS = False
