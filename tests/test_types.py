30a9880c5c33429e9a43126d30550f016dd2ab85
Ivan Krivosheev <py.krivosheev@gmail.com>
2019-12-12 01:06:43 +0300

    Fix retturn type for MultiDictProxy.copy

--- tests/test_types.py
+++ tests/test_types.py
@@ -17,6 +17,18 @@ def test_dict_not_inherited_from_proxy(_multidict):
     assert not issubclass(_multidict.MultiDict, _multidict.MultiDictProxy)
 
 
+def test_multidict_proxy_copy_type(_multidict):
+    d = _multidict.MultiDict(key="val")
+    p = _multidict.MultiDictProxy(d)
+    assert isinstance(p.copy(), _multidict.MultiDict)
+
+
+def test_cimultidict_proxy_copy_type(_multidict):
+    d = _multidict.CIMultiDict(key="val")
+    p = _multidict.CIMultiDictProxy(d)
+    assert isinstance(p.copy(), _multidict.CIMultiDict)
+
+
 def test_create_multidict_proxy_from_nonmultidict(_multidict):
     with pytest.raises(TypeError):
         _multidict.MultiDictProxy({})
