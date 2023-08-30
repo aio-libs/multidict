92c78816e8ccfe3156137e4a0722ceed6506f879
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-08-30 20:16:16 +0300

    Start testing Python 3.12 in CI
    This patch also adds a Python 3.12 Trove classifier to the
    distribution package metadata.

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ args = dict(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 5 - Production/Stable",
     ],
     author="Andrew Svetlov",
