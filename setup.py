e3627edc36cfbc4f01d0554f598863754081689c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-01-02 01:37:01 +0100

    📦🎨 Move package attrs to declarative config
    This cleans up `setup.py`, keeping only things that are hard to move.
    It puts all possible attrs into `setup.cfg`. It doesn't attempt
    making use of `pyproject.toml` for this purpose because it's easier to
    maintain separate configs in dedicated files, on scale.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-import codecs
 import os
 import platform
-import re
 import sys
 
 from setuptools import Extension, setup
@@ -34,65 +32,13 @@ extensions = [
 ]
 
 
-with codecs.open(
-    os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "multidict", "__init__.py"
-    ),
-    "r",
-    "latin1",
-) as fp:
-    try:
-        version = re.findall(r'^__version__ = "([^"]+)"\r?$', fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError("Unable to determine version.")
-
-
-def read(f):
-    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
-
-
-args = dict(
-    name="multidict",
-    version=version,
-    description=("multidict implementation"),
-    long_description=read("README.rst"),
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Development Status :: 5 - Production/Stable",
-    ],
-    author="Andrew Svetlov",
-    author_email="andrew.svetlov@gmail.com",
-    url="https://github.com/aio-libs/multidict",
-    project_urls={
-        "Chat: Gitter": "https://gitter.im/aio-libs/Lobby",
-        "CI: GitHub": "https://github.com/aio-libs/multidict/actions",
-        "Coverage: codecov": "https://codecov.io/github/aio-libs/multidict",
-        "Docs: RTD": "https://multidict.readthedocs.io",
-        "GitHub: issues": "https://github.com/aio-libs/multidict/issues",
-        "GitHub: repo": "https://github.com/aio-libs/multidict",
-    },
-    license="Apache 2",
-    packages=["multidict"],
-    python_requires=">=3.7",
-)
-
 if not NO_EXTENSIONS:
     print("*********************")
     print("* Accelerated build *")
     print("*********************")
-    setup(ext_modules=extensions, **args)
+    setup(ext_modules=extensions)
 else:
     print("*********************")
     print("* Pure Python build *")
     print("*********************")
-    setup(**args)
+    setup()
