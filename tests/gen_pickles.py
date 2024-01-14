d39148fb64267cca0baf8265c2fb1a42e5e31ad8
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-01-14 15:45:21 +0100

    🐛🧪 Fix referencing modules in `gen_pickles`
    Previously, this script didn't work after a blind refactoring.
    It does now.

--- tests/gen_pickles.py
+++ tests/gen_pickles.py
@@ -1,22 +1,26 @@
 import pickle
+from importlib import import_module
+from pathlib import Path
 
-import multidict
+
+TESTS_DIR = Path(__file__).parent.resolve()
 
 
 def write(tag, cls, proto):
     d = cls([("a", 1), ("a", 2)])
     file_basename = f"{cls.__name__.lower()}-{tag}"
-    with open(f"{file_basename}.pickle.{proto}", "wb") as f:
+    with (TESTS_DIR / f"{file_basename}.pickle.{proto}").open("wb") as f:
         pickle.dump(d, f, proto)
 
 
 def generate():
     _impl_map = {
-        "c-extension": multidict._multidict,
-        "pure-python": multidict._multidict_py,
+        "c-extension": "_multidict",
+        "pure-python": "_multidict_py",
     }
     for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-        for tag, impl in _impl_map.items():
+        for tag, impl_name in _impl_map.items():
+            impl = import_module(f"multidict.{impl_name}")
             for cls in impl.CIMultiDict, impl.MultiDict:
                 write(tag, cls, proto)
 
