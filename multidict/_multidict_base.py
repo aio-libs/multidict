bbba084cda3481565157ba60635df50046034b8f
Sam Bull <git@sambull.org>
2025-02-15 14:42:35 +0000

    Test removing redundant code

--- multidict/_multidict_base.py
+++ multidict/_multidict_base.py
@@ -59,48 +59,24 @@ def _viewbaseset_richcmp(view, other, op):
 def _viewbaseset_and(view, other):
     if not isinstance(other, Iterable):
         return NotImplemented
-    if isinstance(view, Set):
-        view = set(iter(view))
-    if isinstance(other, Set):
-        other = set(iter(other))
-    if not isinstance(other, Set):
-        other = set(iter(other))
     return view & other
 
 
 def _viewbaseset_or(view, other):
     if not isinstance(other, Iterable):
         return NotImplemented
-    if isinstance(view, Set):
-        view = set(iter(view))
-    if isinstance(other, Set):
-        other = set(iter(other))
-    if not isinstance(other, Set):
-        other = set(iter(other))
     return view | other
 
 
 def _viewbaseset_sub(view, other):
     if not isinstance(other, Iterable):
         return NotImplemented
-    if isinstance(view, Set):
-        view = set(iter(view))
-    if isinstance(other, Set):
-        other = set(iter(other))
-    if not isinstance(other, Set):
-        other = set(iter(other))
     return view - other
 
 
 def _viewbaseset_xor(view, other):
     if not isinstance(other, Iterable):
         return NotImplemented
-    if isinstance(view, Set):
-        view = set(iter(view))
-    if isinstance(other, Set):
-        other = set(iter(other))
-    if not isinstance(other, Set):
-        other = set(iter(other))
     return view ^ other
 
 
