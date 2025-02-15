3b301e502a4a83aa7642b6463a1cfef8068a9a47
Sam Bull <git@sambull.org>
2025-02-15 15:24:20 +0000

    3.7

--- multidict/_multilib/pythoncapi_compat.h
+++ multidict/_multilib/pythoncapi_compat.h
@@ -552,16 +552,11 @@ PyModule_Add(PyObject *mod, const char *name, PyObject *value)
 // gh-108014 added Py_IsFinalizing() to Python 3.13.0a1
 // bpo-1856 added _Py_Finalizing to Python 3.2.1b1.
 // _Py_IsFinalizing() was added to PyPy 7.3.0.
-#if (0x030201B1 <= PY_VERSION_HEX && PY_VERSION_HEX < 0x030D00A1) \
-        && (!defined(PYPY_VERSION_NUM) || PYPY_VERSION_NUM >= 0x7030000)
+#if (PY_VERSION_HEX < 0x030D00A1) \
+     && (!defined(PYPY_VERSION_NUM) || PYPY_VERSION_NUM >= 0x7030000)
 static inline int Py_IsFinalizing(void)
 {
-#if PY_VERSION_HEX >= 0x030700A1
-    // _Py_IsFinalizing() was added to Python 3.7.0a1.
     return _Py_IsFinalizing();
-#else
-    return (_Py_Finalizing != NULL);
-#endif
 }
 #endif
 
