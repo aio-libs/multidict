78f16545789b6967a9286291f6f66aae65238651
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-03-19 18:16:55 +0100

    tune

--- multidict/_compat.py
+++ multidict/_compat.py
@@ -10,6 +10,6 @@ USE_EXTENSIONS = not NO_EXTENSIONS and not PYPY
 if USE_EXTENSIONS:
     try:
         from . import _multidict  # type: ignore[attr-defined]  # noqa: F401
-    except ImportError:  # pragma: no branch
+    except ImportError:  # pragma: no cover
         # FIXME: Refactor for coverage. See #837.
         USE_EXTENSIONS = False
