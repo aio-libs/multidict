c69186ebf0ca9ef1a24b911e80965147e2c6686b
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-03-21 14:49:50 +0100

    fix

--- multidict/_multidict_base.py
+++ multidict/_multidict_base.py
@@ -1,23 +1,16 @@
 import sys
 from collections.abc import (
-    Container,
     ItemsView,
     Iterable,
     KeysView,
     Sequence,
-    Set,
 )
-from typing import Literal, Union
+from typing import Union
 
 if sys.version_info >= (3, 10):
     from types import NotImplementedType
 else:
     from typing import Any as NotImplementedType
-
-if sys.version_info >= (3, 11):
-    from typing import assert_never
-else:
-    from typing_extensions import assert_never
 
 
 _ViewArg = Union[KeysView[str], ItemsView[str, object]]
@@ -61,19 +54,3 @@ def _viewbaseset_xor(
     lft = set(iter(view))
     rgt = set(iter(other))
     return lft ^ rgt
-
-
-def _itemsview_isdisjoint(view: Container[object], other: Iterable[object]) -> bool:
-    "Return True if two sets have a null intersection."
-    for v in other:
-        if v in view:
-            return False
-    return True
-
-
-def _keysview_isdisjoint(view: Container[object], other: Iterable[object]) -> bool:
-    "Return True if two sets have a null intersection."
-    for k in other:
-        if k in view:
-            return False
-    return True
