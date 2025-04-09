aio-libs/multidict

commit 389758e0b4ad7c5037b56a156106c7e82ad54201

Use istr as keys in CIMultiDict (#1097)
Currently, keys of CIMultiDict are stored as-is, e.g. `CIMultiDict({'k':
'v'})` has `str` key.

But it is slightly incorrect, CIMultiDict should store `istr` keys.

The PR fixes it. The change is backward compatible because `istr` is derived from `str`.

---------

Co-authored-by: J. Nick Koston <nick@koston.org>

multidict/_multilib/istr.h
@@ -28,8 +28,16 @@ istr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     static char *kwlist[] = {"object", "encoding", "errors", 0};
     PyObject *encoding = NULL;
     PyObject *errors = NULL;
-    PyObject *s = NULL;
+    PyObject *canonical = NULL;
     PyObject * ret = NULL;
+    if (kwds != NULL) {
+        int cmp = PyDict_Pop(kwds, multidict_str_canonical, &canonical);
+        if (cmp < 0) {
+            return NULL;
+        } else if (cmp > 0) {
+            Py_INCREF(canonical);
+        }
+    }
 
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOO:str",
                                      kwlist, &x, &encoding, &errors)) {
@@ -43,12 +51,22 @@ istr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     if (!ret) {
         goto fail;
     }
-    s = PyObject_CallMethodNoArgs(ret, multidict_str_lower);
-    if (!s) {
-        goto fail;
+
+    if (canonical == NULL) {
+        canonical = PyObject_CallMethodNoArgs(ret, multidict_str_lower);
+        if (!canonical) {
+            goto fail;
+        }
     }
-    ((istrobject*)ret)->canonical = s;
-    s = NULL;  /* the reference is stollen by .canonical */
+    if (!PyUnicode_CheckExact(canonical)) {
+        PyObject *tmp = PyUnicode_FromObject(canonical);
+        Py_CLEAR(canonical);
+        if (tmp == NULL) {
+            goto fail;
+        }
+        canonical = tmp;
+    }
+    ((istrobject*)ret)->canonical = canonical;
     return ret;
 fail:
     Py_XDECREF(ret);
@@ -98,6 +116,40 @@ static PyTypeObject istr_type = {
     .tp_new = (newfunc)istr_new,
 };
 
+
+static inline PyObject *
+IStr_New(PyObject *str, PyObject *canonical)
+{
+    PyObject *args = NULL;
+    PyObject *kwds = NULL;
+    PyObject *res = NULL;
+
+    args = PyTuple_Pack(1, str);
+    if (args == NULL) {
+        goto ret;
+    }
+
+    if (canonical != NULL) {
+        kwds = PyDict_New();
+        if (kwds == NULL) {
+            goto ret;
+        }
+        if (!PyUnicode_CheckExact(canonical)) {
+            PyErr_SetString(PyExc_TypeError,
+                            "'canonical' argument should be exactly str");
+            goto ret;
+        }
+        if (PyDict_SetItem(kwds, multidict_str_canonical, canonical) < 0) {
+            goto ret;
+        }
+    }
+
+    res = istr_new(&istr_type, args, kwds);
+ret:
+    Py_CLEAR(args);
+    Py_CLEAR(kwds);
+    return res;
+}
 
 static inline int
 istr_init(void)

multidict/_multidict.c
@@ -126,7 +126,7 @@ fail:
 }
 
 
-static inline int
+static inline Py_ssize_t
 _multidict_extend_parse_args(PyObject *args, PyObject *kwds,
                              const char *name, PyObject **parg)
 {
@@ -345,84 +345,12 @@ ret:
 }
 
 static inline PyObject *
-_do_multidict_repr(MultiDictObject *md, PyObject *name,
-                   bool show_keys, bool show_values)
-{
-    PyObject *key = NULL,
-             *value = NULL;
-    bool comma = false;
-
-    PyUnicodeWriter *writer = PyUnicodeWriter_Create(1024);
-    if (writer == NULL)
-        return NULL;
-
-    if (PyUnicodeWriter_WriteChar(writer, '<') <0)
-        goto fail;
-    if (PyUnicodeWriter_WriteStr(writer, name) <0)
-        goto fail;
-    if (PyUnicodeWriter_WriteChar(writer, '(') <0)
-        goto fail;
-
-    pair_list_pos_t pos;
-    pair_list_init_pos(&md->pairs, &pos);
-
-    for (;;) {
-        int res = pair_list_next(&md->pairs, &pos, &key, &value);
-        if (res < 0) {
-            goto fail;
-        }
-        if (res == 0) {
-            break;
-        }
-
-        if (comma) {
-            if (PyUnicodeWriter_WriteChar(writer, ',') <0)
-                goto fail;
-            if (PyUnicodeWriter_WriteChar(writer, ' ') <0)
-                goto fail;
-        }
-        if (show_keys) {
-            if (PyUnicodeWriter_WriteChar(writer, '\'') <0)
-                goto fail;
-            if (PyUnicodeWriter_WriteStr(writer, key) <0)
-                goto fail;
-            if (PyUnicodeWriter_WriteChar(writer, '\'') <0)
-                goto fail;
-        }
-        if (show_keys && show_values) {
-            if (PyUnicodeWriter_WriteChar(writer, ':') <0)
-                goto fail;
-            if (PyUnicodeWriter_WriteChar(writer, ' ') <0)
-                goto fail;
-        }
-        if (show_values) {
-            if (PyUnicodeWriter_WriteRepr(writer, value) <0)
-                goto fail;
-        }
-
-        Py_CLEAR(key);
-        Py_CLEAR(value);
-        comma = true;
-    }
-
-    if (PyUnicodeWriter_WriteChar(writer, ')') <0)
-        goto fail;
-    if (PyUnicodeWriter_WriteChar(writer, '>') <0)
-        goto fail;
-    return PyUnicodeWriter_Finish(writer);
-fail:
-    Py_CLEAR(key);
-    Py_CLEAR(value);
-    PyUnicodeWriter_Discard(writer);
-}
-
-static inline PyObject *
 multidict_repr(MultiDictObject *self)
 {
     PyObject *name = PyObject_GetAttrString((PyObject*)Py_TYPE(self), "__name__");
     if (name == NULL)
         return NULL;
-    PyObject *ret = _do_multidict_repr(self, name, true, true);
+    PyObject *ret = pair_list_repr(&self->pairs, name, true, true);
     Py_CLEAR(name);
     return ret;
 }
@@ -604,9 +532,11 @@ static inline PyObject *
 multidict_extend(MultiDictObject *self, PyObject *args, PyObject *kwds)
 {
     PyObject *arg = NULL;
-    if (_multidict_extend_parse_args(args, kwds, "extend", &arg) < 0) {
+    Py_ssize_t size = _multidict_extend_parse_args(args, kwds, "extend", &arg);
+    if (size < 0) {
         return NULL;
     }
+    pair_list_grow(&self->pairs, size);
     if (_multidict_extend(self, arg, kwds, "extend", 1) < 0) {
         return NULL;
     }
@@ -1200,7 +1130,7 @@ multidict_proxy_repr(MultiDictProxyObject *self)
     PyObject *name = PyObject_GetAttrString((PyObject*)Py_TYPE(self), "__name__");
     if (name == NULL)
         return NULL;
-    PyObject *ret = _do_multidict_repr(self->md, name, true, true);
+    PyObject *ret = pair_list_repr(&self->md->pairs, name, true, true);
     Py_CLEAR(name);
     return ret;
 }
@@ -1412,6 +1342,7 @@ static inline void
 module_free(void *m)
 {
     Py_CLEAR(multidict_str_lower);
+    Py_CLEAR(multidict_str_canonical);
     Py_CLEAR(viewbaseset_and_func);
     Py_CLEAR(viewbaseset_or_func);
     Py_CLEAR(viewbaseset_sub_func);
@@ -1436,6 +1367,10 @@ PyInit__multidict(void)
 {
     multidict_str_lower = PyUnicode_InternFromString("lower");
     if (multidict_str_lower == NULL) {
+        goto fail;
+    }
+    multidict_str_canonical = PyUnicode_InternFromString("_canonical");
+    if (multidict_str_canonical == NULL) {
         goto fail;
     }
 
@@ -1531,6 +1466,7 @@ PyInit__multidict(void)
 
 fail:
     Py_XDECREF(multidict_str_lower);
+    Py_XDECREF(multidict_str_canonical);
 
     return NULL;
 }

multidict/_multidict_py.py
@@ -1,5 +1,6 @@
 import enum
 import sys
+from abc import abstractmethod
 from array import array
 from collections.abc import (
     Callable,
@@ -14,6 +15,7 @@ from typing import (
     TYPE_CHECKING,
     Generic,
     NoReturn,
+    Optional,
     TypeVar,
     Union,
     cast,
@@ -32,6 +34,7 @@ class istr(str):
     """Case insensitive str."""
 
     __is_istr__ = True
+    __istr_title__: Optional[str] = None
 
 
 _V = TypeVar("_V")
@@ -80,8 +83,15 @@ class _Iter(Generic[_T]):
 
 
 class _ViewBase(Generic[_V]):
-    def __init__(self, impl: _Impl[_V]):
+    def __init__(
+        self,
+        impl: _Impl[_V],
+        identfunc: Callable[[str], str],
+        keyfunc: Callable[[str], str],
+    ):
         self._impl = impl
+        self._identfunc = identfunc
+        self._keyfunc = keyfunc
 
     def __len__(self) -> int:
         return len(self._impl._items)
@@ -91,8 +101,13 @@ class _ItemsView(_ViewBase[_V], ItemsView[str, _V]):
     def __contains__(self, item: object) -> bool:
         if not isinstance(item, (tuple, list)) or len(item) != 2:
             return False
+        key, value = item
+        try:
+            ident = self._identfunc(key)
+        except TypeError:
+            return False
         for i, k, v in self._impl._items:
-            if item[0] == k and item[1] == v:
+            if ident == i and value == v:
                 return True
         return False
 
@@ -103,20 +118,20 @@ class _ItemsView(_ViewBase[_V], ItemsView[str, _V]):
         for i, k, v in self._impl._items:
             if version != self._impl._version:
                 raise RuntimeError("Dictionary changed during iteration")
-            yield k, v
+            yield self._keyfunc(k), v
 
     def __repr__(self) -> str:
         lst = []
-        for item in self._impl._items:
-            lst.append("{!r}: {!r}".format(item[1], item[2]))
+        for i, k, v in self._impl._items:
+            lst.append(f"'{k}': {v!r}")
         body = ", ".join(lst)
-        return "<{}({})>".format(self.__class__.__name__, body)
+        return f"<{self.__class__.__name__}({body})>"
 
 
 class _ValuesView(_ViewBase[_V], ValuesView[_V]):
     def __contains__(self, value: object) -> bool:
-        for item in self._impl._items:
-            if item[2] == value:
+        for i, k, v in self._impl._items:
+            if v == value:
                 return True
         return False
 
@@ -124,23 +139,26 @@ class _ValuesView(_ViewBase[_V], ValuesView[_V]):
         return _Iter(len(self), self._iter(self._impl._version))
 
     def _iter(self, version: int) -> Iterator[_V]:
-        for item in self._impl._items:
+        for i, k, v in self._impl._items:
             if version != self._impl._version:
                 raise RuntimeError("Dictionary changed during iteration")
-            yield item[2]
+            yield v
 
     def __repr__(self) -> str:
         lst = []
-        for item in self._impl._items:
-            lst.append("{!r}".format(item[2]))
+        for i, k, v in self._impl._items:
+            lst.append(repr(v))
         body = ", ".join(lst)
-        return "<{}({})>".format(self.__class__.__name__, body)
+        return f"<{self.__class__.__name__}({body})>"
 
 
 class _KeysView(_ViewBase[_V], KeysView[str]):
     def __contains__(self, key: object) -> bool:
-        for item in self._impl._items:
-            if item[1] == key:
+        if not isinstance(key, str):
+            return False
+        identity = self._identfunc(key)
+        for i, k, v in self._impl._items:
+            if i == identity:
                 return True
         return False
 
@@ -148,24 +166,58 @@ class _KeysView(_ViewBase[_V], KeysView[str]):
         return _Iter(len(self), self._iter(self._impl._version))
 
     def _iter(self, version: int) -> Iterator[str]:
-        for item in self._impl._items:
+        for i, k, v in self._impl._items:
             if version != self._impl._version:
                 raise RuntimeError("Dictionary changed during iteration")
-            yield item[1]
+            yield self._keyfunc(k)
 
     def __repr__(self) -> str:
         lst = []
-        for item in self._impl._items:
-            lst.append("{!r}".format(item[1]))
+        for i, k, v in self._impl._items:
+            lst.append(f"'{k}'")
         body = ", ".join(lst)
-        return "<{}({})>".format(self.__class__.__name__, body)
+        return f"<{self.__class__.__name__}({body})>"
+
+
+class _CSMixin:
+    def _key(self, key: str) -> str:
+        return key
+
+    def _title(self, key: str) -> str:
+        if isinstance(key, str):
+            return key
+        else:
+            raise TypeError("MultiDict keys should be either str or subclasses of str")
+
+
+class _CIMixin:
+    def _key(self, key: str) -> str:
+        if type(key) is istr:
+            return key
+        else:
+            return istr(key)
+
+    def _title(self, key: str) -> str:
+        if isinstance(key, istr):
+            ret = key.__istr_title__
+            if ret is None:
+                ret = key.title()
+                key.__istr_title__ = ret
+            return ret
+        if isinstance(key, str):
+            return key.title()
+        else:
+            raise TypeError("MultiDict keys should be either str or subclasses of str")
 
 
 class _Base(MultiMapping[_V]):
     _impl: _Impl[_V]
 
-    def _title(self, key: str) -> str:
-        return key
+    @abstractmethod
+    def _key(self, key: str) -> str: ...
+
+    @abstractmethod
+    def _title(self, key: str) -> str: ...
 
     @overload
     def getall(self, key: str) -> list[_V]: ...
@@ -226,15 +278,15 @@ class _Base(MultiMapping[_V]):
 
     def keys(self) -> KeysView[str]:
         """Return a new view of the dictionary's keys."""
-        return _KeysView(self._impl)
+        return _KeysView(self._impl, self._title, self._key)
 
     def items(self) -> ItemsView[str, _V]:
         """Return a new view of the dictionary's items *(key, value) pairs)."""
-        return _ItemsView(self._impl)
+        return _ItemsView(self._impl, self._title, self._key)
 
     def values(self) -> _ValuesView[_V]:
         """Return a new view of the dictionary's values."""
-        return _ValuesView(self._impl)
+        return _ValuesView(self._impl, self._title, self._key)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Mapping):
@@ -266,11 +318,11 @@ class _Base(MultiMapping[_V]):
         return False
 
     def __repr__(self) -> str:
-        body = ", ".join("'{}': {!r}".format(k, v) for k, v in self.items())
-        return "<{}({})>".format(self.__class__.__name__, body)
+        body = ", ".join(f"'{k}': {v!r}" for i, k, v in self._impl._items)
+        return f"<{self.__class__.__name__}({body})>"
 
 
-class MultiDict(_Base[_V], MutableMultiMapping[_V]):
+class MultiDict(_CSMixin, _Base[_V], MutableMultiMapping[_V]):
     """Dictionary with the support for duplicate keys."""
 
     def __init__(self, arg: MDArg[_V] = None, /, **kwargs: _V):
@@ -286,18 +338,9 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
     def __reduce__(self) -> tuple[type[Self], tuple[list[tuple[str, _V]]]]:
         return (self.__class__, (list(self.items()),))
 
-    def _title(self, key: str) -> str:
-        return key
-
-    def _key(self, key: str) -> str:
-        if isinstance(key, str):
-            return key
-        else:
-            raise TypeError("MultiDict keys should be either str or subclasses of str")
-
     def add(self, key: str, value: _V) -> None:
         identity = self._title(key)
-        self._impl._items.append((identity, self._key(key), value))
+        self._impl._items.append((identity, key, value))
         self._impl.incr_version()
 
     def copy(self) -> Self:
@@ -322,8 +365,11 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
         method: Callable[[list[tuple[str, str, _V]]], None],
     ) -> None:
         if arg:
-            if isinstance(arg, (MultiDict, MultiDictProxy)) and not kwargs:
+            if isinstance(arg, (MultiDict, MultiDictProxy)):
                 items = arg._impl._items
+                if kwargs:
+                    for key, value in kwargs.items():
+                        items.append((self._title(key), key, value))
             else:
                 if hasattr(arg, "keys"):
                     arg = cast(SupportsKeys[_V], arg)
@@ -338,20 +384,21 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
                             f"multidict update sequence element #{pos}"
                             f"has length {len(item)}; 2 is required"
                         )
-                    items.append((self._title(item[0]), self._key(item[0]), item[1]))
+                    items.append((self._title(item[0]), item[0], item[1]))
 
             method(items)
         else:
             method(
                 [
-                    (self._title(key), self._key(key), value)
+                    (self._title(key), key, value)
                     for key, value in kwargs.items()
                 ]
             )
 
     def _extend_items(self, items: Iterable[tuple[str, str, _V]]) -> None:
         for identity, key, value in items:
-            self.add(key, value)
+            self._impl._items.append((identity, key, value))
+        self._impl.incr_version()
 
     def clear(self) -> None:
         """Remove all items from MultiDict."""
@@ -456,9 +503,9 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
     def popitem(self) -> tuple[str, _V]:
         """Remove and return an arbitrary (key, value) pair."""
         if self._impl._items:
-            i = self._impl._items.pop()
+            i, k, v = self._impl._items.pop()
             self._impl.incr_version()
-            return i[1], i[2]
+            return self._key(k), v
         else:
             raise KeyError("empty multidict")
 
@@ -499,7 +546,6 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
         self._impl.incr_version()
 
     def _replace(self, key: str, value: _V) -> None:
-        key = self._key(key)
         identity = self._title(key)
         items = self._impl._items
 
@@ -527,47 +573,41 @@ class MultiDict(_Base[_V], MutableMultiMapping[_V]):
                 i += 1
 
 
-class CIMultiDict(MultiDict[_V]):
+class CIMultiDict(_CIMixin, MultiDict[_V]):
     """Dictionary with the support for duplicate case-insensitive keys."""
 
-    def _title(self, key: str) -> str:
-        return key.title()
 
-
-class MultiDictProxy(_Base[_V]):
+class MultiDictProxy(_CSMixin, _Base[_V]):
     """Read-only proxy for MultiDict instance."""
 
     def __init__(self, arg: Union[MultiDict[_V], "MultiDictProxy[_V]"]):
         if not isinstance(arg, (MultiDict, MultiDictProxy)):
             raise TypeError(
                 "ctor requires MultiDict or MultiDictProxy instance"
-                ", not {}".format(type(arg))
+                f", not {type(arg)}"
             )
 
         self._impl = arg._impl
 
     def __reduce__(self) -> NoReturn:
-        raise TypeError("can't pickle {} objects".format(self.__class__.__name__))
+        raise TypeError(f"can't pickle {self.__class__.__name__} objects")
 
     def copy(self) -> MultiDict[_V]:
         """Return a copy of itself."""
         return MultiDict(self.items())
 
 
-class CIMultiDictProxy(MultiDictProxy[_V]):
+class CIMultiDictProxy(_CIMixin, MultiDictProxy[_V]):
     """Read-only proxy for CIMultiDict instance."""
 
     def __init__(self, arg: Union[MultiDict[_V], MultiDictProxy[_V]]):
         if not isinstance(arg, (CIMultiDict, CIMultiDictProxy)):
             raise TypeError(
                 "ctor requires CIMultiDict or CIMultiDictProxy instance"
-                ", not {}".format(type(arg))
+                f", not {type(arg)}"
             )
 
         self._impl = arg._impl
-
-    def _title(self, key: str) -> str:
-        return key.title()
 
     def copy(self) -> CIMultiDict[_V]:
         """Return a copy of itself."""

multidict/_multilib/defs.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 static PyObject *multidict_str_lower = NULL;
+static PyObject *multidict_str_canonical = NULL;
 
 /* We link this module statically for convenience.  If compiled as a shared
    library instead, some compilers don't allow addresses of Python objects

multidict/_multilib/dict.h
@@ -18,11 +18,6 @@ typedef struct {
 } MultiDictProxyObject;
 
 
-static inline PyObject *
-_do_multidict_repr(MultiDictObject *md, PyObject *name,
-                   bool show_keys, bool show_values);
-
-
 #ifdef __cplusplus
 }
 #endif

commit 78761cf0efdbd69d1caa72e37fc997f67aef0809

Fix memory leak creating new istr objects (#1133)
Fixes leak 1 from
https://github.com/aio-libs/multidict/issues/1131#issuecomment-2787514071
(`IStr_New` path)

see
https://github.com/aio-libs/multidict/issues/1131#issuecomment-2787878877
https://github.com/aio-libs/multidict/issues/1131#issuecomment-2787879594
https://github.com/aio-libs/multidict/issues/1131#issuecomment-2787891048

This also seems to fix the perf regression in popitem from
https://github.com/aio-libs/multidict/pull/1097#issuecomment-2746161895
https://codspeed.io/aio-libs/multidict/branches/istr-keys

introduced #1097
fixes #1131

---------

Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

multidict/_multilib/istr.h
@@ -27,31 +27,20 @@ istr_dealloc(istrobject *self)
 }
 
 static inline PyObject *
-istr_new_with_state(PyTypeObject *type, PyObject *args, PyObject *kwds,
-                    mod_state *state)
+istr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
-    if (state == NULL) {
-        PyObject *mod = PyType_GetModuleByDef(type, &multidict_module);
-        if (mod == NULL) {
-            return NULL;
-        }
-        state = get_mod_state(mod);
+    PyObject *mod = PyType_GetModuleByDef(type, &multidict_module);
+    if (mod == NULL) {
+        return NULL;
     }
+    mod_state *state = get_mod_state(mod);
 
     PyObject *x = NULL;
     static char *kwlist[] = {"object", "encoding", "errors", 0};
     PyObject *encoding = NULL;
     PyObject *errors = NULL;
     PyObject *canonical = NULL;
-    PyObject * ret = NULL;
-    if (kwds != NULL) {
-        int cmp = PyDict_Pop(kwds, state->str_canonical, &canonical);
-        if (cmp < 0) {
-            return NULL;
-        } else if (cmp > 0) {
-            Py_INCREF(canonical);
-        }
-    }
+    PyObject *ret = NULL;
 
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOO:str",
                                      kwlist, &x, &encoding, &errors)) {
@@ -65,20 +54,9 @@ istr_new_with_state(PyTypeObject *type, PyObject *args, PyObject *kwds,
     if (!ret) {
         goto fail;
     }
-
-    if (canonical == NULL) {
-        canonical = PyObject_CallMethodNoArgs(ret, state->str_lower);
-        if (!canonical) {
-            goto fail;
-        }
-    }
-    if (!PyUnicode_CheckExact(canonical)) {
-        PyObject *tmp = PyUnicode_FromObject(canonical);
-        Py_CLEAR(canonical);
-        if (tmp == NULL) {
-            goto fail;
-        }
-        canonical = tmp;
+    canonical = PyObject_CallMethodNoArgs(ret, state->str_lower);
+    if (!canonical) {
+        goto fail;
     }
     ((istrobject*)ret)->canonical = canonical;
     ((istrobject*)ret)->state = state;
@@ -86,12 +64,6 @@ istr_new_with_state(PyTypeObject *type, PyObject *args, PyObject *kwds,
 fail:
     Py_XDECREF(ret);
     return NULL;
-}
-
-static inline PyObject *
-istr_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
-{
-    return istr_new_with_state(type, args, kwds, NULL);
 }
 
 static inline PyObject *
@@ -147,33 +119,20 @@ static inline PyObject *
 IStr_New(mod_state *state, PyObject *str, PyObject *canonical)
 {
     PyObject *args = NULL;
-    PyObject *kwds = NULL;
     PyObject *res = NULL;
-
     args = PyTuple_Pack(1, str);
     if (args == NULL) {
         goto ret;
     }
-
-    if (canonical != NULL) {
-        kwds = PyDict_New();
-        if (kwds == NULL) {
-            goto ret;
-        }
-        if (!PyUnicode_CheckExact(canonical)) {
-            PyErr_SetString(PyExc_TypeError,
-                            "'canonical' argument should be exactly str");
-            goto ret;
-        }
-        if (PyDict_SetItem(kwds, state->str_canonical, canonical) < 0) {
-            goto ret;
-        }
+    res = PyUnicode_Type.tp_new(state->IStrType, args, NULL);
+    if (!res) {
+        goto ret;
     }
-
-    res = istr_new_with_state(state->IStrType, args, kwds, state);
+    Py_INCREF(canonical);
+    ((istrobject*)res)->canonical = canonical;
+    ((istrobject*)res)->state = state;
 ret:
     Py_CLEAR(args);
-    Py_CLEAR(kwds);
     return res;
 }
 