fdb402e9fa9b8d5593560aee59c6e97c27388882
Vizonex <Vizonex7@gmail.com>
2025-06-19 11:16:52 -0500

    thanks asvetlov, I downloaded clang-format just to get this passed :)

--- multidict/_multilib/capsule.h
+++ multidict/_multilib/capsule.h
@@ -10,65 +10,76 @@ extern "C" {
 #endif
 
 typedef struct _multidict_capi {
+    // md_get_all
+    int (*_MultiDict_GetAll)(MultiDictObject *self, PyObject *key,
+                             PyObject **ret);
+    // md_get_one
+    int (*_MultiDict_GetOne)(MultiDictObject *self, PyObject *key,
+                             PyObject **ret);
 
-	// md_get_all
-	int (*_MultiDict_GetAll)(MultiDictObject *self, PyObject *key, PyObject **ret);
-	// md_get_one
-	int (*_MultiDict_GetOne)(MultiDictObject *self, PyObject *key, PyObject **ret);
+    // multidict_keys
+    PyObject *(*_MultiDict_Keys)(MultiDictObject *self);
 
-	// multidict_keys
-	PyObject *(*_MultiDict_Keys)(MultiDictObject *self);
+    // multidict_items
+    PyObject *(*_MultiDict_Items)(MultiDictObject *self);
 
-	// multidict_items
-	PyObject *(*_MultiDict_Items)(MultiDictObject *self);
+    // multidict_values
+    PyObject *(*_MultiDict_Values)(MultiDictObject *self);
 
-	// multidict_values
-	PyObject *(*_MultiDict_Values)(MultiDictObject *self);
+    // md_add
+    int (*_MultiDict_Add)(MultiDictObject *self, PyObject *key,
+                          PyObject *value);
 
-	// md_add
-	int (*_MultiDict_Add)(MultiDictObject *self, PyObject *key, PyObject *value);
+    // multidict_clear
+    PyObject *(*_MultiDict_Clear)(MultiDictObject *self);
 
-	// multidict_clear
-	PyObject *(*_MultiDict_Clear)(MultiDictObject *self);
+    // multidict_extend
+    PyObject *(*_MultiDict_Extend)(MultiDictObject *self, PyObject *args,
+                                   PyObject *kwargs);
 
-	// multidict_extend
-	PyObject *(*_MultiDict_Extend)(MultiDictObject *self, PyObject *args, PyObject *kwargs);
+    // multidict_copy
+    PyObject *(*_MultiDict_Copy)(MultiDictObject *self);
 
-	// multidict_copy
-	PyObject *(*_MultiDict_Copy)(MultiDictObject *self);
+    // multidict_setdefault
+    PyObject *(*_MultiDict_SetDefault)(MultiDictObject *self, PyObject *key,
+                                       PyObject *value);
 
-	// multidict_setdefault
-	PyObject *(*_MultiDict_SetDefault)(MultiDictObject *self, PyObject *key, PyObject *value);
+    // md_pop_one
+    int (*_MultiDict_PopOne)(MultiDictObject *self, PyObject *key,
+                             PyObject **ret);
 
-	// md_pop_one
-	int (*_MultiDict_PopOne)(MultiDictObject *self, PyObject *key, PyObject **ret);
+    // md_pop_all
+    int (*_MultiDict_PopAll)(MultiDictObject *self, PyObject *key,
+                             PyObject **ret);
 
-	// md_pop_all
-	int (*_MultiDict_PopAll)(MultiDictObject *self, PyObject *key, PyObject **ret);
+    // md_pop_item
+    PyObject *(*_MultiDict_PopItem)(MultiDictObject *self);
 
-	// md_pop_item
-	PyObject *(*_MultiDict_PopItem)(MultiDictObject *self);
+    // multidict_update
+    PyObject *(*_MultiDict_Update)(MultiDictObject *self, PyObject *args,
+                                   PyObject *kwds);
 
-	// multidict_update
-	PyObject *(*_MultiDict_Update)(MultiDictObject *self, PyObject *args, PyObject *kwds);
+    // TODO:
+    // PyObject *(*_MultiDict_Delete)(MultiDictObject *self, PyObject *);
 
-	// 
-	PyObject* (*_MultiDict_Delete)(MultiDictObject* self, PyObject* );
-	
 } MultiDict_CAPI;
 
 static MultiDict_CAPI *MultiDictAPI = NULL;
 
-static int MultiDict_IMPORT() {
-	MultiDictAPI = PyCapsule_Import("multidict._multidict.multidict_CAPI", 0);
-	return (MultiDictAPI != NULL) ? 0 : -1;
+static int
+MultiDict_IMPORT()
+{
+    MultiDictAPI = PyCapsule_Import("multidict._multidict.multidict_CAPI", 0);
+    return (MultiDictAPI != NULL) ? 0 : -1;
 }
 
-/*********************  MultiDict / CIMultiDict Macros  *********************/
+/*  MultiDict / CIMultiDict Macros  */
 
-#define MultiDict_GetAll(self, key, ret) MultiDictAPI->_MultiDict_GetAll(self, key, ret)
+#define MultiDict_GetAll(self, key, ret) \
+    MultiDictAPI->_MultiDict_GetAll(self, key, ret)
 
-#define MultiDict_GetOne(self, key, ret) MultiDictAPI->_MultiDict_GetOne(self, key, ret)
+#define MultiDict_GetOne(self, key, ret) \
+    MultiDictAPI->_MultiDict_GetOne(self, key, ret)
 
 #define MultiDict_Keys(self) MultiDictAPI->_MultiDict_Keys(self)
 
@@ -76,32 +87,39 @@ static int MultiDict_IMPORT() {
 
 #define MutliDict_Items(self) MultiDictAPI->_MultiDict_Items(self)
 
-#define MultiDict_Add(self, key, value) MultiDictAPI->_MultiDict_Add(self, key, value)
+#define MultiDict_Add(self, key, value) \
+    MultiDictAPI->_MultiDict_Add(self, key, value)
 
 #define MultiDict_Clear(self) MultiDictAPI->_MultiDict_Clear(self)
 
-#define MultiDict_Extend(self, args, kwargs) MultiDictAPI->_MultiDict_Extend(self, args, kwargs)
+#define MultiDict_Extend(self, args, kwargs) \
+    MultiDictAPI->_MultiDict_Extend(self, args, kwargs)
 
 #define MultiDict_Copy(self) MultiDictAPI->_MultiDict_Copy(self)
 
-#define MultiDict_SetDefault(self, key, value) MultiDictAPI->_MultiDict_SetDefault(self, key, value)
+#define MultiDict_SetDefault(self, key, value) \
+    MultiDictAPI->_MultiDict_SetDefault(self, key, value)
 
-#define MultiDict_PopOne(self, key, ret) MultiDictAPI->_MultiDict_PopOne(self, key, ret)
+#define MultiDict_PopOne(self, key, ret) \
+    MultiDictAPI->_MultiDict_PopOne(self, key, ret)
 
 #define MultiDict_PopItem(self) MultiDictAPI->_MultiDict_PopItem(self)
 
-#define MultiDict_Update(self, args, kwargs) MultiDictAPI->_MultiDict_Update(self, args, kwargs)
+#define MultiDict_Update(self, args, kwargs) \
+    MultiDictAPI->_MultiDict_Update(self, args, kwargs)
 
+/* MultiDictProxy / CIMultiDictProxy Macros */
 
-/*********************  MultiDictProxy / CIMultiDictProxy Macros  *********************/
+#define MultiDictProxy_GetAll(self, key, ret) \
+    MultiDictAPI->_MultiDict_GetAll(self->md, key, ret)
 
-#define MultiDictProxy_GetAll(self, key, ret) MultiDictAPI->_MultiDict_GetAll(self->md, key, ret)
-
-#define MultiDictProxy_GetOne(self, key, ret) MultiDictAPI->_MultiDict_GetOne(self->md, key, ret)
+#define MultiDictProxy_GetOne(self, key, ret) \
+    MultiDictAPI->_MultiDict_GetOne(self->md, key, ret)
 
 // NOTE: MultiDictProxy_Get will be going in the __init__.pxd file
 
-#define MultiDictProxy_Keys(self, key) MultiDictAPI->_MultiDict_Keys(self->md, key)
+#define MultiDictProxy_Keys(self, key) \
+    MultiDictAPI->_MultiDict_Keys(self->md, key)
 
 #define MutliDictProxy_Values(self) MultiDictAPI->_MultiDict_Values(self->md)
 
@@ -110,13 +128,8 @@ static int MultiDict_IMPORT() {
 #define MultiDictProxy_Copy(self) MultiDictAPI->_MultiDict_Copy(self->md)
 
 
-
-// TODO: (Vizonex) I would like to add IStr next
-// so we can speed up aiohttp's http-writer
-
-
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __CAPSULE_H__
+#endif  // __CAPSULE_H__
