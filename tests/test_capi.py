3d2e83a7fb36e7c690c05e89dbf904116a9f6992
Vizonex <Vizonex7@gmail.com>
2025-06-21 09:47:39 -0500

    add test_del

--- tests/test_capi.py
+++ tests/test_capi.py
@@ -25,3 +25,27 @@ def test_md_add() -> None:
     testcapi.md_add(md, "key", "value")
     assert len(md) == 1
     assert list(md.items()) == [("key", "value")]
+
+def test_md_clear() -> None:
+    previous = multidict.MultiDict([("key", "value")])
+    md: MultiDictStr = previous.copy()
+    testcapi.md_clear(md)
+    assert md != previous
+
+def test_set_default() -> None:
+    md: MultiDictStr = multidict.MultiDict([("key", "one"), ("key", "two")], foo="bar")
+    assert "one" == testcapi.md_set_default(md, "key", "three")
+    assert "three" == testcapi.md_set_default(md, "otherkey", "three")
+    assert "otherkey" in md
+    assert "three" == md["otherkey"]
+
+def test_del() -> None:
+    d = multidict.MultiDict([("key", "one"), ("key", "two")], foo="bar")
+    assert list(d.keys()) == ["key", "key", "foo"]
+
+    testcapi.md_del(d, "key")
+    assert d == {"foo": "bar"}
+    assert list(d.items()) == [("foo", "bar")]
+
+    with pytest.raises(KeyError, match="key"):
+        testcapi.md_del(d, "key")
