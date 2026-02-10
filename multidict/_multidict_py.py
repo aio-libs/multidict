4383844d821ff093763109c21d63c62778afb407
Varun Chawla <varun_6april@hotmail.com>
2026-02-10 02:48:54 -0800

    Fix missing space in ValueError message in _parse_args

--- multidict/_multidict_py.py
+++ multidict/_multidict_py.py
@@ -831,7 +831,7 @@ class MultiDict(_CSMixin, MutableMultiMapping[_V]):
                 for pos, item in enumerate(arg):
                     if not len(item) == 2:
                         raise ValueError(
-                            f"multidict update sequence element #{pos}"
+                            f"multidict update sequence element #{pos} "
                             f"has length {len(item)}; 2 is required"
                         )
                     identity = identity_func(item[0])
