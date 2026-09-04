299943e07d79ea142258a2c90c83f2c5bdc90dc4
Andrew Svetlov <andrew.svetlov@gmail.com>
2026-09-04 10:41:56 +0200

    Rename config file

--- docs/conf.py
+++ docs/conf.py
@@ -208,7 +208,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [alabaster.get_path()]  # type: ignore[no-untyped-call]
+html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
