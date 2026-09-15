e21f62808430ef247e71478ad423cc9098a1a3d7
J. Nick Koston <nick@home-assistant.io>
2026-09-15 02:25:50 -0500

    Read the keys once under the critical section in __sizeof__

--- multidict/_multidict.c
+++ multidict/_multidict.c
@@ -1264,12 +1264,15 @@ static PyObject*
 multidict_sizeof(MultiDictObject* self)
 {
     Py_ssize_t size = sizeof(MultiDictObject);
-    if (self->keys != &empty_htkeys) {
-        size += htkeys_sizeof(self->keys);
-        if (self->keys->tails != NULL) {
-            size += (Py_ssize_t)htkeys_tails_bytes(self->keys->log2_size);
+    Py_BEGIN_CRITICAL_SECTION(self);
+    htkeys_t* keys = self->keys;
+    if (keys != &empty_htkeys) {
+        size += htkeys_sizeof(keys);
+        if (keys->tails != NULL) {
+            size += (Py_ssize_t)htkeys_tails_bytes(keys->log2_size);
         }
     }
+    Py_END_CRITICAL_SECTION();
     return PyLong_FromSsize_t(size);
 }
 
